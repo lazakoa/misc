b7fc371f24c3122112aa1ba42215e0f6e12f6b6f
Andrei Lazakovitch <alazakovitch@gmail.com>
2022-04-07 15:34:21 -0400

    Finished 79.

--- euler-py/079.py
+++ euler-py/079.py
@@ -16,8 +16,67 @@ passcode of unknown length.
 
 f = open('resources/keylog.txt', 'r')
 
-chars = []
+sequences = []
 
 for line in f:
-    chars.append(int(line))
+    i1, i2 , i3 , nl = line
+    sequences.append((i1, i2, i3))
+
+def reverseVerify(sequence, passcode):
+
+    counter = 0;
+    temp = passcode #map(int, list(str(passcode)))
+
+    for number in temp:
+        if number == sequence[counter]:
+            counter += 1
+            if counter == 3:
+                return True
+
+    return False
+
+def verifyFactory(sequence):
+
+    def temp(passcode):
+        return reverseVerify(sequence, passcode)
+
+    return temp
+
+funcList = list(map(verifyFactory, sequences))
+
+"""
+Brute force failed, second approach is to look at what digits appear
+inside the keylog file and construct all possible numbers from the set
+of valid digits.
+"""
+
+def validDigits(sequences):
+
+    result = set()
+
+    for sequence in sequences:
+        for number in sequence:
+            result.add(number)
+
+    return result
+
+from itertools import product
+
+"""
+for perm in product(validDigits(sequences), repeat=8):
+
+    tally = []
+
+    for func in funcList:
+
+        if func(perm) == True:
+            tally.append(True)
+
+        if len(tally) == 50:
+            print(perm)
+"""
+
+# Ran above code to find the following:
+# 73162893 doesn't work, cause i'm a idiot and used ints instead of str
+print("73162890")
 
