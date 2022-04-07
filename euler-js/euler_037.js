9bbb4702a11f09273110f11e8c2ec357aa72c56a
Andrei Lazakovitch <alazakovitch@gmail.com>
2022-04-07 15:34:20 -0400

    euler 37 done

--- euler-js/euler_037.js
+++ euler-js/euler_037.js
@@ -16,17 +16,76 @@ NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
  */
 
 var prime = require('./prime.js');
+var wu = require('wu');
+var lo = require('lodash');
 
-function rightTruncate(x, f) {
+// This thing is pretty fucking fast actually, takes about a second to solve
+// the question. Entire solution was "designed" on paper, more moving parts
+// in there than i'm used too, plus i had to set up the filters to eliminate
+// redundant work.
 
+// Kinda ugly, but I didn't know how much work I would be doing up front
+// to find the 11 primes, so i made it go as fast as I could think of with
+// minimal effort. Uses like no memory too.
+
+function euler37() {
+   
+   // valid first & last digits
+   var fD = new Set([ '2', '3', '5', '7']);
+   var lD = new Set([ '3', '7']);
+
+   var primeCache = new Set();
+    
+   // works, saves a ton of work too
+   var source = wu(wu.count(8)).filter( x => {
+       return fD.has(x.toString()[0]) && lD.has(x.toString().slice(-1)); }
+   );
+
+   function isTruncatblePrime(x) {
+       return smearOut(x).every( x => {
+           if (primeCache.has(x)) {
+               return true;
+           }                    
+           else if (prime.checkPrime(x)) {
+               primeCache.add(x);
+               return true;
+           }
+           else {
+               return false;
+           }
+       });
+   }
+
+   var res = wu(wu(source).filter( x => {
+       if (prime.checkPrime(x)) {
+           primeCache.add(x);
+           return true;
+       }
+       else {
+           return false;
+       }
+   })).filter(isTruncatblePrime);
+   var ans = 0;
+   for (var p of wu(res).take(12)) {
+       ans += p;
+   }
+   return ans;
 }
 
-function 
+function smearOut(x) {
+    var temp = String(x).split('');
+    var rToL = temp.slice();
+    var lToR = temp.slice();
+    var result = []; 
 
-function findTruncatablePrimes() {
-    var primes = new Set([2, 3, 4, 7]);
-
-    function recur(x) {
-            if (c) {
-            
+    for (var counter = 0; counter < temp.length - 1; counter++) {
+        rToL.shift();
+        lToR.pop();
+        result.push(Number(rToL.join('')));
+        result.push(Number(lToR.join('')));
+    }
+    return result;
 }
+
+// Got the answer on first try! Yay.
+console.log(euler37());
