108302779cbf47876ce2a499379ed886932e0e9b
Andrei Lazakovitch <alazakovitch@gmail.com>
2022-04-07 15:34:20 -0400

    euler 11 done with js

--- euler-js/euler_011.js
+++ euler-js/euler_011.js
@@ -32,12 +32,13 @@ What is the greatest product of four adjacent numbers in any direction
 */
 
 var lo = require('lodash');
+var buckets = require('buckets-js');
 
 var grid = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\
             49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\
             81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\
             52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\
-            22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\ 
+            22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\
             24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\
             32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\
             67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\
@@ -54,4 +55,103 @@ var grid = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\
             20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\
             01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
 
+var cgrid = lo.filter(grid.split('  '), (x) => { return x !== '';});
 
+cgrid = lo.map(cgrid, (x) => { return x.split(' ').map(Number); });
+
+// cgrid = lo.reduce(cgrid, (a, b) => { return a.concat(b);});
+
+function dirVec(grid, i, j, dx, dy, length) {
+    var temp = []; // starting point
+    for (var counter = 0 ; counter < length; counter++) {
+        temp.push(grid[i + dy * counter][j + dx * counter]);
+    }
+    return temp;
+}
+
+function transpose(grid) {
+    // transposes an n-array of n-array
+    var temp = lo.map(grid, (_ , index) => { return lo.map(grid,
+                                        (array) => { return array[index]; });});
+    return temp;
+}    
+
+// didn't end up using the function below, wait I did.
+
+function combinations(array, n) {
+    // Returns every possible n,n-1,n-2,. ..-tuple that can be constructed 
+    // from elements in array, This is so ugly I want to cry & all because js 
+    // has no immutable tuples. I could do this with raw objects I think? 
+    
+    var temp = new Set();
+
+    function recur(s, array, dimensions ) {
+        if (dimensions === 0) {
+            return s;
+        }
+        else {
+            for (var elem in array) {
+                var value = recur(s + ',' + String(array[elem]), 
+                                array, dimensions - 1);
+                temp.add(value);
+            }
+        }
+        return s;
+    }
+    recur('', array, n);
+    
+    var output = [];
+    for (let e of temp) {
+        output.push(e.slice(1).split(',').map(Number));
+    }
+    // ugliness ends here
+    
+    return output;
+}
+
+function permutations(array, n) {
+    return lo.filter(combinations(array, n), (x) => { return x.length ===  n });
+}
+
+// skips the some elements in the horizontal direction in a band on top and
+// on the bottom, will be added if needed to finish the problem. Also skips 
+// the vertical bands on the left and right sides.
+
+function traverse(grid, n) {
+    var max = 0;
+    for (var i = n - 1; i < grid.length - n; i++) {
+        for (var j = n - 1; j < grid.length - n; j++) {
+            var temp =  lo.max(lo.map(lo.filter(permutations([-1, 0, 1], 2),  
+                (x) => { return x.toString() !== [0,0].toString()}).map(
+                    (y) => { return dirVec(cgrid, i, j, y[0], y[1], 4);}),
+            (nVec) => { return lo.reduce(nVec, (a, b) => {return a * b;});}));
+
+            if (temp >= max) {
+                max = temp;
+            }
+        }
+    }
+    return max;
+}
+
+/*
+lo.max(lo.map(lo.filter(permutations([-1, 0, 1], 2),  
+                (x) => { return x.toString() !== [0,0].toString()}).map(
+                    (y) => { return dirVec(cgrid, 3, 3, y[0], y[1], 4);}),
+            (nVec) => { return lo.reduce(nVec, (a, b) => {return a * b;});}));
+*/
+
+
+traverse(cgrid, 4);
+
+traverse(transpose(cgrid, 4));
+
+// the function above took too long to write for a completely pointless reason,
+// forgot to close a statement somewhere and node accepted it without errors, 
+// all my time with js so far seems to be spent hunting down stuff like this 
+// over and over again
+//
+
+// didn't end up needing the transpose function
+
+console.log(traverse(cgrid, 4));
