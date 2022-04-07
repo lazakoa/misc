6f6b4c7ed0e766197835227d2224b1d8a286d779
Andrei Lazakovitch <alazakovitch@gmail.com>
2022-04-07 15:34:20 -0400

    70 turned out to be suprisingly easy, but I did crash my computer once doing it.

--- euler-py/070.py
+++ euler-py/070.py
@@ -21,8 +21,52 @@ and the ratio n/f(n) produces a minimum.
         * Calculate all totients for 1 < n < 10**7
     * Return the n for which n/f(n) is minimized while satisfying the 
     permutation property.
+    * Can't do this naively, naive method for earlier problem took 1 minute.
+    And given what i suspect about the order of growth I don't want to try 
+    running this for 2 to 10**7.
 """  
 
 """
-    * Generate all primes s.t p < 10**7
+    * n/phi(n) is minimized when n is a prime since phi(p) = p - 1 for p a
+    prime.
+    * Check if there is a prime number that satisfies this what is required.
+    If there is, then that is the solution.
+    * If above doesn't work, then it is time to start being clever. A seive 
+    of some sort would work, but would be a pain.
+    * Won't work p - 1 is never a permutation of p
+    * phi(n) becomes larger when n has less prime factors, and their occurence
+    is kept to a minimum.
+    * Use a sieve to generate all primes s.t p < 10**7 
+    * Make a (p1,p2) combination using elements of that sieve
+    * Filter it down & see if you get an answer
+    * Since we are using pairs, we only need 10**7 // 2 primes
 """
+
+from itertools import combinations
+from pyprimes import primes_below
+from functools import reduce
+
+def is_permutation(n1, n2):
+    n1 = sorted(list(str(n1)))
+    n2 = sorted(list(str(n2)))
+    return n1 == n2
+
+bound = 10**7
+
+# since the ratio is minimized when the prime factors are approxmiately of 
+# the same order, we don't need to generate pair wise combinations over all 
+# primes up to 10 mil.
+
+ans = reduce(lambda x,y: x if x[0] < y[0] else y,
+    map(lambda x: ((x[0]*x[1])/((x[0] - 1)*(x[1] - 1)), x),
+        filter(lambda x: is_permutation(x[0]*x[1], (x[0] - 1)*(x[1] - 1)),
+            filter(lambda x: (x[0] * x[1]) < bound,
+                combinations(primes_below(10**5 // 2), 2)))))
+
+print(ans[1][0] * ans[1][1])
+
+
+
+
+
+
